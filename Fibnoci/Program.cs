7504e5018b301d1c481340fb2f897a3d1abbce56
Abhishek Narwariya <abhisheknarwariya813@gmail.com>
2022-05-10 11:01:02 +0530

    Adding Fibnocci

---
+++ Fibnoci/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace LogicalPrograms
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int a = 0;
+            int b = 1;
+            int c;
+
+           
+            Console.WriteLine("Enter number");
+            int number = Convert.ToInt32(Console.ReadLine());
+          
+            for (int i = 0; i < number; i++)
+            {
+                c = a + b;
+                Console.WriteLine(c);
+
+             
+                a = b;
+                b = c;
+            }
+            Console.ReadLine();
+        }
+    }
+}
