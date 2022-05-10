4cf480cd98388c30bb16ac36dd19c0ce39932745
Abhishek Narwariya <abhisheknarwariya813@gmail.com>
2022-05-10 13:03:50 +0530

    Adding StopWatch

---
+++ StopWatch/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Diagnostics;
+
+namespace LogicalPrograms
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Stopwatch stopwatch = new Stopwatch();
+
+           
+            stopwatch.Start();
+
+            
+            for (int i = 0; i < 1000; i++)
+            {
+                Thread.Sleep(1);
+            }
+ 
+            stopwatch.Stop();
+            Console.WriteLine("Time elapsed:{0}", stopwatch.Elapsed);
+            Console.ReadLine();
+        }
+    }
+}
